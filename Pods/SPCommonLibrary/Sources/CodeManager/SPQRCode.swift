787ff41a97618d66b6cfa158ccfee0008b9b4b6a
黄树鹏 <825844231@qq.com>
2020-01-15 17:38:30 +0800

    提交appstore V1.0.5 升级code2020011501

--- Pods/SPCommonLibrary/Sources/CodeManager/SPQRCode.swift
+++ Pods/SPCommonLibrary/Sources/CodeManager/SPQRCode.swift
@@ -11,13 +11,11 @@ import AVFoundation
 /// 二维码管理
 public class SPQRCode {
     /// 创建二维码
-    ///
     /// - Parameters:
     ///   - qrCode: 二维码内容
     ///   - iconImg: 中心的图片
     ///   - size: 二维码大小
     ///   - iconSize: 中心图片的大小
-    /// - Returns: 二维码图片 
     public class func sp_create(qrCode:String,iconImg : UIImage? = nil,size : CGSize = CGSize(width: 300, height: 300),iconSize : CGSize = CGSize(width: 30, height: 30))->UIImage?{
         if let filter = CIFilter(name: "CIQRCodeGenerator") {
             filter.setDefaults()
@@ -36,20 +34,16 @@ public class SPQRCode {
         return nil
     }
     /// 添加中间图片
-    ///
     /// - Parameters:
     ///   - image: 图片
     ///   - iconImg: 中间logo图片
     ///   - iconSize: 终极logo图片的大小
-    /// - Returns: 转换后的图片
     public class func sp_center(image : UIImage,iconImg : UIImage?,iconSize : CGSize  = CGSize(width: 30, height: 30))->UIImage?{
         return image.sp_image(centerImg: iconImg, iconSize: iconSize)
     }
     
     /// 从图片中识别二维码内容
-    ///
     /// - Parameter codeImg: 二维码图片
-    /// - Returns: 识别到的数据
     public class func sp_recognizeQRCode(codeImg : UIImage?)-> [String]?{
         guard let img = codeImg  else {
             return nil
@@ -73,12 +67,10 @@ public class SPQRCode {
     }
     
     /// 修改二维码图片的颜色
-    ///
     /// - Parameters:
     ///   - color1: 二维码内容图片颜色
     ///   - bgColor: 二维码背景颜色
     ///   - image: 二维码图片
-    /// - Returns: 转换后的图片
     public class func sp_color(color1 : UIColor,bgColor : UIColor?,image : UIImage) ->UIImage?{
         if let filter = CIFilter(name: "CIFalseColor"){
             filter.setDefaults()
@@ -98,13 +90,11 @@ public class SPQRCode {
         return image
     }
     /// 添加背景图片
-    ///
     /// - Parameters:
     ///   - bgImg: 背景图片
     ///   - image: 原始图片
     ///   - minHueAngle: 去掉背景颜色最低的颜色值HSV
     ///   - maxHueAngle: 去掉背景颜色最高的颜色值HSV
-    /// - Returns:  转换后的图片
     public class func sp_add(bgImg : UIImage,image:UIImage,minHueAngle:Float,maxHueAngle:Float) -> UIImage?{
         let cubeMap = createCubeMap(minHueAngle, maxHueAngle)
         let data = NSData(bytesNoCopy: cubeMap.data, length: Int(cubeMap.length), freeWhenDone: true)
